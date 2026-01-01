b6b8a1ee0e9a61613e203e1237308354a8579c97
mseffine <seffinemohamed470@gmail.com>
2026-01-01 11:28:01 +0100

    init the app

---
+++ backend/moblogging/src/main/java/com/zone01oujda/moblogging/MobloggingApplication.java
@@ -0,0 +1,13 @@
+package com.zone01oujda.moblogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobloggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobloggingApplication.class, args);
+	}
+
+}
---
+++ backend/moblogging/src/test/java/com/zone01oujda/moblogging/MobloggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zone01oujda.moblogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobloggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
